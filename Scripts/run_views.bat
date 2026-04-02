b8d66f354b9925ea42132dad520513248d686657
Andrea Morales Vega <morales.vega.andrea@gmailcom>
2026-04-02 15:42:07 -0600

    Version #23 02/04/2026

--- Scripts/run_views.bat
+++ Scripts/run_views.bat
@@ -1,6 +1,5 @@
 docker exec -i culturatrip_db psql -U culturatrip -d culturatrip -f /docker-entrypoint-initdb.d/04_new_tables.sql
 docker exec -i culturatrip_db psql -U culturatrip -d culturatrip -f /docker-entrypoint-initdb.d/05_new_views.sql
 docker exec -i culturatrip_db psql -U culturatrip -d culturatrip -f /docker-entrypoint-initdb.d/06_index.sql
-docker exec -i culturatrip_db psql -U culturatrip -d culturatrip -f /docker-entrypoint-initdb.d/07_new_changes.sql
 docker exec -i culturatrip_db psql -U culturatrip -d culturatrip -f /docker-entrypoint-initdb.d/08_alter_tables.sql
 docker exec -i culturatrip_db psql -U culturatrip -d culturatrip -f /docker-entrypoint-initdb.d/09_ML_views.sql
